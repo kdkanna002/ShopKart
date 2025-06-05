985f9b1e2d57d185f003455f6c84ab0358e7bfc0
kdkanna002 <kdkanna002@gmail.com>
2025-06-05 23:58:54 +0530

    Configured settings.py for deployment

--- Dk_project/settings.py
+++ Dk_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('django-insecure-5o#mk4nn$@uzw+$xu&2+@z+m80qwx*_uabp$i$0wgcff(ett1b')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
